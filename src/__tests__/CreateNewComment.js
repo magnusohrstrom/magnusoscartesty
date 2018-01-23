730e1590f72b0c0e159bd87c9358be817c0fded5
magnusohrstrom <magnusohrstrom330@gmail.com>
2018-01-23 14:02:11 +0100

    ååååskarr

---
+++ src/__tests__/CreateNewComment.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, shallow, mount } from 'enzyme';
+import CreateNewComment from '../components/CreateNewComment';
+import Comments from '../components/Comments';
+
+it('should set state comments on change of inputvalue for textarea and create new comment on form submit', () => {
+    
+    const wrapperComments = mount(<Comments/>);
+    const wrapperCreate = mount(<CreateNewComment 
+        author ="Zac"
+        postId = "asfks"
+        updateComments = {wrapperComments.instance()
+            .setCommentsFromLocalStorage}/>);
+    
+    console.log(wrapperCreate.props());
+    const text = wrapperCreate.find('#comment');
+    text.simulate('change', { target : { name: "comment", value : "ÅÅÅÅÅSKAAAAR" }});
+    expect(wrapperCreate.state().comment).toEqual("ÅÅÅÅÅSKAAAAR");
+    const button = wrapperCreate.find('form');
+    button.simulate('submit');
+    expect(wrapperComments.state().comments[0]
+        .comment).toEqual('ÅÅÅÅÅSKAAAAR');
+})  
+
