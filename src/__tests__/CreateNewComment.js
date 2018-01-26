6d709e84fcd91c3fb63cc0b68725b6514e5f90b6
magnusohrstrom <magnusohrstrom330@gmail.com>
2018-01-26 14:19:16 +0100

    false

--- src/__tests__/CreateNewComment.js
+++ src/__tests__/CreateNewComment.js
@@ -19,5 +19,24 @@ it('should set state comments on change of inputvalue for textarea and create ne
     button.simulate('submit');
     expect(wrapperComments.state().comments[0]
         .comment).toEqual('ÅÅÅÅÅSKAAAAR');
-})  
+});
+
+
+it('should set state comments on change of inputvalue for false value', () => {
+    
+    const wrapperComments = mount(<Comments/>);
+    const wrapperCreate = mount(<CreateNewComment 
+        author ="Zac"
+        postId = "asfks"
+        updateComments = {wrapperComments.instance()
+            .setCommentsFromLocalStorage}/>);
+    
+    const text = wrapperCreate.find('#comment');
+    text.simulate('change', { target : { name: "comment", value : false }});
+    expect(wrapperCreate.state().comment).toEqual(false);
+    const button = wrapperCreate.find('form');
+    button.simulate('submit');
+    expect(wrapperComments.state().comments[1]
+        .comment).toEqual(false);
+});
 
